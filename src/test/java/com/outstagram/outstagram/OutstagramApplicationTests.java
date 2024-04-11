9a5999fec6167d76af4bf2f929f50b80a1adc7bf
ikhyeon <andantej281@gmail.com>
2024-04-11 11:11:39 +0900

    init: project setting

---
+++ src/test/java/com/outstagram/outstagram/OutstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outstagram.outstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
