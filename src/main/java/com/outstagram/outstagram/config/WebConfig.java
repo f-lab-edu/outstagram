06a63cf003bd7c539683c61c15149fad8303f153
ikhyeon <andantej281@gmail.com>
2024-04-17 13:00:21 +0900

    feat: @Login AOP 개발 완료 - argumentResolver 대신에 AOP로 @Login 애노테이션 처리 구현

--- src/main/java/com/outstagram/outstagram/config/WebConfig.java
+++ src/main/java/com/outstagram/outstagram/config/WebConfig.java
@@ -2,10 +2,7 @@ package com.outstagram.outstagram.config;
 
 import com.outstagram.outstagram.common.filter.LoggingInterceptor;
 import com.outstagram.outstagram.common.filter.LoginCheckInterceptor;
-import com.outstagram.outstagram.resolver.LoginMemberArgumentResolver;
-import java.util.List;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,13 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
             );
     }
 
-
-    /**
-     * LoginMemberArgumentResolver 등록
-     */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver());
-    }
+//    /**
+//     * LoginMemberArgumentResolver 등록
+//     */
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(new LoginMemberArgumentResolver());
+//    }
 
 }
