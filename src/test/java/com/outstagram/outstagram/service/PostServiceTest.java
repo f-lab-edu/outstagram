97f1d0c242d23702c44f6661aff74a174c6fef1a
ikhyeon <andantej281@gmail.com>
2024-04-25 19:20:16 +0900

    test: 게시물 CRUD 및 좋아요 관련 테스트 코드 개발 완료 - 나의 게시물 조회, 게시물 수정 및 삭제 기능 테스트 코드 - 좋아요 누르기 및 취소 기능 테스트 코드 - 내가 좋아요 누른 게시물 조회 기능 테스트 코드

--- src/test/java/com/outstagram/outstagram/service/PostServiceTest.java
+++ src/test/java/com/outstagram/outstagram/service/PostServiceTest.java
@@ -1,25 +1,38 @@
 package com.outstagram.outstagram.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.outstagram.outstagram.controller.request.PostCreateReq;
+import com.outstagram.outstagram.controller.request.PostEditReq;
+import com.outstagram.outstagram.controller.response.MyPostsRes;
 import com.outstagram.outstagram.controller.response.PostRes;
 import com.outstagram.outstagram.dto.ImageDTO;
 import com.outstagram.outstagram.dto.PostDTO;
+import com.outstagram.outstagram.dto.PostImageDTO;
 import com.outstagram.outstagram.dto.UserDTO;
 import com.outstagram.outstagram.exception.ApiException;
 import com.outstagram.outstagram.exception.errorcode.ErrorCode;
 import com.outstagram.outstagram.mapper.PostMapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -36,14 +49,15 @@ class PostServiceTest {
     @Mock
     private UserService userService;
 
+    @Mock
+    private LikeService likeService;
+
     @Test
     public void testInsertPost_Success() {
         // given
         MultipartFile mockFile = mock(MultipartFile.class);
-        PostCreateReq postCreateReq = PostCreateReq.builder()
-                .contents("게시물 내용입니다.")
-                .imgFiles(List.of(mockFile))
-                .build();
+        PostCreateReq postCreateReq = PostCreateReq.builder().contents("게시물 내용입니다.")
+            .imgFiles(List.of(mockFile)).build();
         Long userId = 1L;
 
         // when
@@ -65,18 +79,11 @@ class PostServiceTest {
     public void testGetPost_Success() {
         Long userId = 1L;
         Long postId = 1L;
-        PostDTO post = PostDTO.builder()
-                .id(postId)
-                .userId(userId)
-                .contents("test post contents")
-                .likes(100)
-                .build();
+        PostDTO post = PostDTO.builder().id(postId).userId(userId).contents("test post contents")
+            .likes(100).build();
 
-        UserDTO user = UserDTO.builder()
-                .id(userId)
-                .nickname("testNickname")
-                .imgUrl("www.userImageUrl.com")
-                .build();
+        UserDTO user = UserDTO.builder().id(userId).nickname("testNickname")
+            .imgUrl("www.userImageUrl.com").build();
 
         List<ImageDTO> imageList = Arrays.asList(ImageDTO.builder().postId(postId).build());
 
@@ -100,11 +107,159 @@ class PostServiceTest {
 
         when(postMapper.findById(postId)).thenReturn(null);
 
-        ApiException apiException = assertThrows(ApiException.class, () -> postService.getPost(postId, userId));
+        ApiException apiException = assertThrows(ApiException.class,
+            () -> postService.getPost(postId, userId));
 
         assertEquals(ErrorCode.POST_NOT_FOUND, apiException.getErrorCode());
         assertEquals("해당 게시물은 존재하지 않습니다.", apiException.getDescription());
 
+    }
+
+    @Test
+    public void testGetMyPosts() {
+        Long userId = 1L;
+        List<PostImageDTO> mockPostImages = Arrays.asList(
+            PostImageDTO.builder()
+                .id(1L)
+                .contents("content01")
+                .userId(userId)
+                .imgPath("path01")
+                .savedImgName("savedImgName01")
+                .build(),
+            PostImageDTO.builder()
+                .id(2L)
+                .contents("content02")
+                .userId(userId)
+                .imgPath("path02")
+                .savedImgName("savedImgName02")
+                .build()
+        );
+
+        when(postMapper.findWithImageByUserId(userId)).thenReturn(mockPostImages);
+        when(likeService.existsLike(userId, 1L)).thenReturn(true);
+        when(likeService.existsLike(userId, 2L)).thenReturn(false);
+
+        List<MyPostsRes> myPosts = postService.getMyPosts(userId);
+
+        assertNotNull(myPosts);
+        assertEquals(mockPostImages.size(), myPosts.size());
+        assertEquals(
+            mockPostImages.get(0).getImgPath() + "\\" + mockPostImages.get(0).getSavedImgName(),
+            myPosts.get(0).getThumbnailUrl()
+        );
+        assertEquals(
+            mockPostImages.get(1).getImgPath() + "\\" + mockPostImages.get(1).getSavedImgName(),
+            myPosts.get(1).getThumbnailUrl()
+        );
+        assertTrue(myPosts.get(0).getIsLiked());
+        assertFalse(myPosts.get(1).getIsLiked());
+
+        verify(postMapper).findWithImageByUserId(userId);
+        verify(likeService).existsLike(userId, 1L);
+        verify(likeService).existsLike(userId, 2L);
+    }
+
+    @Test
+    public void testEditPost() {
+        Long userId = 1L;
+        Long postId = 1L;
+        PostDTO mockPost = PostDTO.builder().id(postId).userId(userId).contents("before contents")
+            .build();
+
+        PostEditReq postEditReq = PostEditReq.builder().contents("after contents")
+            .deleteImgIds(new ArrayList<>()).build();
+
+        when(postMapper.findById(postId)).thenReturn(mockPost);
+        when(postMapper.updateContentsById(postId, "after contents")).thenReturn(1);
+
+        assertDoesNotThrow(() -> postService.editPost(postId, postEditReq, userId));
+        verify(postMapper).updateContentsById(postId, "after contents");
+    }
+
+    @Test
+    public void testDeletePost() {
+        Long userId = 1L;
+        Long postId = 1L;
+        PostDTO mockPost = PostDTO.builder().id(postId).userId(userId).contents("contents").build();
+
+        when(postMapper.findById(postId)).thenReturn(mockPost);
+        when(postMapper.deleteById(postId)).thenReturn(1);
+
+        assertDoesNotThrow(() -> postService.deletePost(postId, userId));
+        verify(postMapper).deleteById(postId);
+    }
+
+    @Test
+    public void testIncreaseLike() {
+        Long postId = 1L;
+        Long userId = 1L;
+
+        when(postMapper.updateLikeCount(postId, 1)).thenReturn(1);
+
+        assertDoesNotThrow(() -> postService.increaseLike(postId, userId));
+        verify(postMapper).updateLikeCount(postId, 1);
+        verify(likeService).insertLike(userId, postId);
+    }
+
+    @Test
+    public void testUnlikePost() {
+        Long postId = 1L;
+        Long userId = 1L;
+
+        when(postMapper.updateLikeCount(postId, -1)).thenReturn(1);
+
+        assertDoesNotThrow(() -> postService.unlikePost(postId, userId));
+        verify(postMapper).updateLikeCount(postId, -1);
+        verify(likeService).deleteLike(userId, postId);
+
+    }
+
+    @Test
+    public void testGetLikePosts() {
+        Long userId = 1L;
+        List<Long> mockLikePostIds = Arrays.asList(2L, 3L, 4L);
+        List<PostImageDTO> mockPostImages = Arrays.asList(
+            PostImageDTO.builder()
+                .id(2L)
+                .contents("content02")
+                .likes(100)
+                .userId(userId)
+                .imgPath("path02")
+                .savedImgName("savedImgName02")
+                .build(),
+            PostImageDTO.builder()
+                .id(3L)
+                .contents("content03")
+                .likes(9999)
+                .userId(userId)
+                .imgPath("path03")
+                .savedImgName("savedImgName03")
+                .build(),
+            PostImageDTO.builder()
+                .id(4L)
+                .contents("content04")
+                .likes(36)
+                .userId(userId)
+                .imgPath("path04")
+                .savedImgName("savedImgName04")
+                .build()
+        );
+
+        when(likeService.getLikePosts(userId)).thenReturn(mockLikePostIds);
+        when(postMapper.findLikePostsWithImageByPostIds(mockLikePostIds)).thenReturn(mockPostImages);
+
+        List<MyPostsRes> likePosts = postService.getLikePosts(userId);
+
+        assertNotNull(likePosts);
+        assertEquals(
+            mockPostImages.get(0).getImgPath() + "\\" + mockPostImages.get(0).getSavedImgName(),
+            likePosts.get(0).getThumbnailUrl()
+        );
+        assertTrue(likePosts.get(0).getIsLiked());
+        assertEquals(mockPostImages.get(1).getLikes(), likePosts.get(1).getLikes());
+
+        verify(likeService).getLikePosts(userId);
+        verify(postMapper).findLikePostsWithImageByPostIds(mockLikePostIds);
     }
 
 }
