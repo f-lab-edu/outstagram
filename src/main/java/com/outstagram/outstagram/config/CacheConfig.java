167038efce9c275c83afd8bdc114c3b1ce7397c0
ikhyeon <andantej281@gmail.com>
2024-06-04 10:35:12 +0900

    fix: redis 역직렬화 오류 해결 - java 8의 LocalDateTime 역직렬화 오류 해결 - LinkedHashMap -> 객체 역직렬화 오류 해결 - 둘 다 objectMapper 설정으로 해결함

--- src/main/java/com/outstagram/outstagram/config/CacheConfig.java
+++ src/main/java/com/outstagram/outstagram/config/CacheConfig.java
@@ -1,5 +1,10 @@
 package com.outstagram.outstagram.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.outstagram.outstagram.common.constant.CacheNames;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -50,8 +55,20 @@ public class CacheConfig {
     @Bean
     public RedisCacheManager cacheManager(@Qualifier("redisCacheConnectionFactory") RedisConnectionFactory connectionFactory) {
 
+        // Jackson 라이브러리가 Java8의 LocalDateTime 타입을 직렬화할 수 없는 오류 해결하기 위한 objectMapper 설정 코드
+        // LinkedHashMap을 다른 타입으로 casting 할 수 없는 오류 해결하기 위해 DefaultTyping 활성화
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+            .allowIfSubType(Object.class)
+            .build();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
 
         Map<String, RedisCacheConfiguration> redisCacheConfigMap = new HashMap<>();
         redisCacheConfigMap.put(CacheNames.POST, defaultConfig.entryTtl(Duration.ofHours(1)));
