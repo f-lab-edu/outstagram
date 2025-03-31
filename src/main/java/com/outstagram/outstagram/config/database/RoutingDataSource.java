6a93259e78c3023a657c033ef7170be96e8edb5f
nick <andantej281@gmail.com>
2025-03-31 15:42:34 +0900

    feat: 현재 데이터 소스 확인을 위한 로그 추가

--- src/main/java/com/outstagram/outstagram/config/database/RoutingDataSource.java
+++ src/main/java/com/outstagram/outstagram/config/database/RoutingDataSource.java
@@ -1,11 +1,16 @@
 package com.outstagram.outstagram.config.database;
 
+import com.outstagram.outstagram.config.database.DatabaseContextHolder.DatabaseType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+@Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        return DatabaseContextHolder.getDatabaseType();
+        DatabaseType type = DatabaseContextHolder.getDatabaseType();
+        log.info("========== DB 라우팅 : {}", type);
+        return type;
     }
 }
